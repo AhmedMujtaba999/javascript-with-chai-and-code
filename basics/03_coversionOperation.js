8b86a2646428a331838fa1260c80fcc830031306
ahmed <mujtabaaliahmed559@gmail.com>
2026-02-27 00:04:39 +0530

    operation in js

--- basics/03_coversionOperation.js
+++ basics/03_coversionOperation.js
@@ -30,3 +30,53 @@ console.log(typeof (score));
     let stringsomething = String(something) // '90' conversion to string
     console.log(stringsomething); // '90'
     console.log(typeof (stringsomething)); // string
+
+
+    /// -------------------------Operations-------------------------
+
+    let value = 30
+    let negvalue = -value
+    console.log(negvalue); // -30
+    let isloggedin1 = true
+    let isloggedin2 = false
+    console.log(isloggedin1);
+    console.log(isloggedin2);
+    console.log(!isloggedin1); // false
+    console.log(!isloggedin2); // true
+
+    let str1 = "Hello"
+    let str2 = "World"
+    let str3 = str1 + " " + str2 // concatenation operation
+    console.log(str3); // Hello World
+
+    console.log(5 + '10' +2 ); // 5102 because of the left to right evaluation and the first operand is a string so it will convert the second operand to a string and concatenate them and then it will convert the third operand to a string and concatenate it with the previous result
+    console.log(5 + 10 + '2' ); // 152
+    console.log('5' + 10 + 2 ); // 5102
+    let num1,num2,num3;
+    num1 = num2 = num3 = 2+1 // right to left evaluation
+    console.log(num1);
+    // comparisiom
+    console.log(3 > 2); // true
+    console.log(3 < 2); // false
+    console.log(3 >= 2); // true
+    console.log(3 <= 2); // false
+    console.log(3 == 2);
+    console.log("2">1 );
+    console.log("02"<1 );
+    // null value comparison
+
+    console.log(null > 0); // false
+    console.log(null === 0);
+    console.log(null >= 0); // true
+    console.log(null <= 0);
+
+    console.log(undefined > 0); // false
+    console.log(undefined === 0);
+
+    console.log(undefined >= 0); // false
+    console.log(undefined <= 0); // false
+
+    console.log(null>"23");
+    console.log(undefined <"234");
+    
+    // === strict check (also checks the type of the value)
